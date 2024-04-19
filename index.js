6d26e56a906b986e8753ec79c778a8433e2afeca
Louis Bompart <lbompart@coveo.com>
2024-04-19 16:15:06 -0400

    debug

---
+++ index.js
@@ -0,0 +1,10 @@
+import { spawnSync } from "child_process";
+
+const ps = spawnSync("npm.ps1");
+
+if (ps.error) {
+  console.error("Error:", ps.error);
+}
+
+const whereNpm = spawnSync("where", ["npm"]);
+console.log("whereNpm:", whereNpm.stdout.toString());
